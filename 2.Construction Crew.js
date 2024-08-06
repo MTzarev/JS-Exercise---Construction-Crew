ce68e79d52471eec640bca328f4d56bb298ba293
Miroslav Tsarev <mtzarev@gmail.com>
2024-08-06 23:39:35 +0300

    Update 2.Construction Crew.js

--- 2.Construction Crew.js
+++ 2.Construction Crew.js
@@ -10,13 +10,4 @@ console.log(constructionCrew({ weight: 80,
     levelOfHydrated: 0,
     dizziness: true }
   ));
-//   constructionCrew({ weight: 120,
-//     experience: 20,
-//     levelOfHydrated: 200,
-//     dizziness: true }
-//   );
-//   constructionCrew({ weight: 95,
-//     experience: 3,
-//     levelOfHydrated: 0,
-//     dizziness: false }
-//   );
+
